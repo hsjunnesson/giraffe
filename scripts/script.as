10d155306daf1db444b4e88e4bf918ab1d994f17
Hans Sjunnesson <hans.sjunnesson@fatshark.se>
2023-11-07 22:15:06 +0100

    work

--- scripts/script.as
+++ scripts/script.as
@@ -1,9 +1,70 @@
+
+class Mob {
+    float mass = 100.0f;
+    glm::vec2 position = glm::vec2(0.0f, 0.0f);
+    glm::vec2 velocity = glm::vec2(0.0f, 0.0f);
+    glm::vec2 steering_direction = glm::vec2(0.0f, 0.0f);
+    glm::vec2 steering_target = glm::vec2(0.0f, 0.0f);
+    float max_force = 30.0f;
+    float max_speed = 30.0f;
+    float orientation = 0.0f;
+    float radius = 10.0f;
+}
+
+class Giraffe {
+    uint64 sprite_id = 0;
+    Mob mob;
+    bool dead = false;
+}
+
+class Lion {
+    uint64 sprite_id = 0;
+    Mob mob;
+    Giraffe@ locked_giraffe = null;
+    float energy = 0.0f;
+    float max_energy = 10.0f;
+}
+
+struct Obstacle {
+    glm::vec2 position = glm::vec2(0.0f, 0.0f);
+    float radius = 100.0f;
+    math::Color4f color = math::Color4f(1.0f, 1.0f, 1.0f, 1.0f);
+};
+
+struct Food {
+    uint64 sprite_id = 0;
+    glm::vec2 position = glm::vec2(0.0f, 0.0f);
+};
+
+void on_enter(engine::Engine@ engine, game::Game@ game) {
+}
+
+void on_leave(engine::Engine@ engine, game::Game@ game) {
+}
+
+// void on_input(engine::Engine@ engine, game::Game@ game, engine::InputCommand input_command) {
+// }
+
 void update(engine::Engine@ engine, game::Game@ game, float t, float dt) {
 //    engine::Sprites@ sprites = game.sprites;
 //	engine::update_sprites(sprites, t, dt);
 
-	glm::vec2 a;
-	glm::vec2 b(1.0f, 2.0f);
-	glm::vec2 c = a + b;
+	// glm::vec2 a;
+    // a.x = 5.5;
+	// glm::vec2 b(1.0f, 2.0f);
+	// glm::vec2 c = a + b;
+    // print(c.tostring());
 
+    // glm::vec2 d = c / 2.0;
+    // print(d.tostring());
+
+    // glm::vec2 n = glm::normalize(d);
+    // print(n.tostring());
+    // print("" + glm::length(n));
+}
+
+void render(engine::Engine@ engine, game::Game@ game) {
+}
+
+void render_imgui(engine::Engine@ engine, game::Game@ game) {
 }
--- src/angelscript_game.cpp
+++ src/angelscript_game.cpp
@@ -29,7 +29,12 @@
 
 namespace {
 asIScriptEngine *script_engine = nullptr;
+asIScriptFunction *on_enter_func = nullptr;
+asIScriptFunction *on_leave_func = nullptr;
+asIScriptFunction *on_input_func = nullptr;
 asIScriptFunction *update_func = nullptr;
+asIScriptFunction *render_func = nullptr;
+asIScriptFunction *render_imgui_func = nullptr;
 asIScriptContext *ctx = nullptr;
 }
 
@@ -53,22 +58,39 @@ void update_sprites_wrapper(engine::Sprites *sprites, float t, float dt) {
     engine::update_sprites(*sprites, t, dt);
 }
 
-glm::vec2 add_vec2(const glm::vec2& a, const glm::vec2& b) {
+glm::vec2 add_vec2(const glm::vec2 a, const glm::vec2 b) {
     return a + b;
 }
 
-glm::vec2 sub_vec2(const glm::vec2& a, const glm::vec2& b) {
+glm::vec2 sub_vec2(const glm::vec2 a, const glm::vec2 b) {
     return a - b;
 }
 
-void ConstructVec2(float x, float y, void *memory) {
+void vec2_default(void *memory) {
+    new(memory) glm::vec2();
+}
+
+void vec2_construct(float x, float y, void *memory) {
     new(memory) glm::vec2(x, y);
 }
 
-void DestructVec2(void *memory) {
-//    ((glm::vec2*)memory)->~vec2();
+std::string glm_vec2_tostring(const glm::vec2 &v) {
+    std::ostringstream oss;
+    oss << "vec2(" << v.x << ", " << v.y << ")";
+    return oss.str();
 }
 
+glm::vec2 vec2_normalize(const glm::vec2 &v) {
+    return glm::normalize(v);
+}
+
+float vec2_length(const glm::vec2 &v) {
+    return glm::length(v);
+}
+
+float vec2_length2(const glm::vec2 &v) {
+    return glm::length2(v);
+}
 
 namespace angelscript {
 
@@ -135,23 +157,27 @@ void initialize(foundation::Allocator &allocator) {
 		r = script_engine->SetDefaultNamespace("glm");
 		assert(r >= 0);
 
-		r = script_engine->RegisterObjectType("vec2", sizeof(glm::vec2), asOBJ_VALUE | asGetTypeTraits<glm::vec2>());
-		assert(r >= 0);
+		r = script_engine->RegisterObjectType("vec2", sizeof(glm::vec2), asOBJ_VALUE | asOBJ_POD | asOBJ_APP_CLASS_C | asOBJ_APP_CLASS_ALLFLOATS); assert(r >= 0);
 
-		r = script_engine->RegisterObjectBehaviour("vec2", asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(ConstructVec2), asCALL_CDECL_OBJLAST);
-		assert(r >= 0);
+		r = script_engine->RegisterObjectProperty("vec2", "float x", asOFFSET(glm::vec2, x)); assert(r >= 0);
+		r = script_engine->RegisterObjectProperty("vec2", "float y", asOFFSET(glm::vec2, y)); assert(r >= 0);
 
-		r = script_engine->RegisterObjectBehaviour("vec2", asBEHAVE_CONSTRUCT, "void f(float, float)", asFUNCTION(ConstructVec2), asCALL_CDECL_OBJLAST);
-		assert(r >= 0);
+		r = script_engine->RegisterObjectBehaviour("vec2", asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(vec2_default), asCALL_CDECL_OBJLAST); assert(r >= 0);
+		r = script_engine->RegisterObjectBehaviour("vec2", asBEHAVE_CONSTRUCT, "void f(float, float)", asFUNCTION(vec2_construct), asCALL_CDECL_OBJLAST); assert(r >= 0);
 
-		r = script_engine->RegisterObjectBehaviour("vec2", asBEHAVE_DESTRUCT, "void f()", asFUNCTION(DestructVec2), asCALL_CDECL_OBJLAST);
-		assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 opAdd(const vec2 &in) const", asFUNCTIONPR(glm::operator+, (const glm::vec2&, const glm::vec2&), glm::vec2), asCALL_CDECL_OBJFIRST); assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 opSub(const vec2 &in) const", asFUNCTIONPR(glm::operator-, (const glm::vec2&, const glm::vec2&), glm::vec2), asCALL_CDECL_OBJFIRST); assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 opMul(const vec2 &in) const", asFUNCTIONPR(glm::operator*, (const glm::vec2&, const glm::vec2&), glm::vec2), asCALL_CDECL_OBJFIRST); assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 opMul(const float) const", asFUNCTIONPR(glm::operator*, (const glm::vec2&, const float), glm::vec2), asCALL_CDECL_OBJFIRST); assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 opDiv(const vec2 &in) const", asFUNCTIONPR(glm::operator/, (const glm::vec2&, const glm::vec2&), glm::vec2), asCALL_CDECL_OBJFIRST); assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 opDiv(const float) const", asFUNCTIONPR(glm::operator/, (const glm::vec2&, const float), glm::vec2), asCALL_CDECL_OBJFIRST); assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "vec2 &opAssign(const vec2 &in)", asMETHODPR(glm::vec2, operator=, (const glm::vec2&), glm::vec2&), asCALL_THISCALL); assert(r >= 0);
 
-		r = script_engine->RegisterGlobalFunction("vec2 AddVec2(const vec2 &in, const vec2 &in)", asFUNCTION(add_vec2), asCALL_CDECL);
-		assert(r >= 0);
+        r = script_engine->RegisterObjectMethod("vec2", "string tostring() const", asFUNCTION(glm_vec2_tostring), asCALL_CDECL_OBJFIRST); assert(r >= 0);
 
-		r = script_engine->RegisterGlobalFunction("vec2 SubVec2(const vec2 &in, const vec2 &in)", asFUNCTION(sub_vec2), asCALL_CDECL);
-		assert(r >= 0);
+        r = script_engine->RegisterGlobalFunction("vec2 normalize(const vec2 &in)", asFUNCTION(vec2_normalize), asCALL_CDECL); assert(r >= 0);
+        r = script_engine->RegisterGlobalFunction("float length(const vec2 &in)", asFUNCTION(vec2_length), asCALL_CDECL); assert(r >= 0);
+        r = script_engine->RegisterGlobalFunction("float length2(const vec2 &in)", asFUNCTION(vec2_length2), asCALL_CDECL); assert(r >= 0);
 
 		r = script_engine->SetDefaultNamespace("");
 		assert(r >= 0);
@@ -180,9 +206,16 @@ void initialize(foundation::Allocator &allocator) {
     assert(r >= 0);
 
     // Get functions
-    
+    on_enter_func = mod->GetFunctionByDecl("void on_enter(engine::Engine@ engine, game::Game@ game)");
+    on_leave_func = mod->GetFunctionByDecl("void on_enter(engine::Engine@ engine, game::Game@ game)");
     update_func = mod->GetFunctionByDecl("void update(engine::Engine@ engine, game::Game@ game, float t, float dt)");
+    render_func = mod->GetFunctionByDecl("void render(engine::Engine@ engine, game::Game@ game)");
+    render_imgui_func = mod->GetFunctionByDecl("void render_imgui(engine::Engine@ engine, game::Game@ game)");
+    assert(on_enter_func);
+    assert(on_leave_func);
     assert(update_func);
+    assert(render_func);
+    assert(render_imgui_func);
 }
 
 void close() {
@@ -204,9 +237,29 @@ namespace game {
 
 
 void game_state_playing_enter(engine::Engine &engine, Game &game) {
+    if (ctx && update_func) {
+        ctx->Prepare(on_enter_func);
+        ctx->SetArgAddress(0, &engine);
+        ctx->SetArgAddress(1, &game);
+        int r = ctx->Execute();
+        if (r != asEXECUTION_FINISHED) {
+            log_fatal("Could not execute on_enter() in scripts/script.as: %s", ctx->GetExceptionString());
+        }
+        ctx->Unprepare();
+    }
 }
 
 void game_state_playing_leave(engine::Engine &engine, Game &game) {
+    if (ctx && update_func) {
+        ctx->Prepare(on_leave_func);
+        ctx->SetArgAddress(0, &engine);
+        ctx->SetArgAddress(1, &game);
+        int r = ctx->Execute();
+        if (r != asEXECUTION_FINISHED) {
+            log_fatal("Could not execute on_leave() in scripts/script.as: %s", ctx->GetExceptionString());
+        }
+        ctx->Unprepare();
+    }
 }
 
 void game_state_playing_on_input(engine::Engine &engine, Game &game, engine::InputCommand &input_command) {
@@ -228,9 +281,29 @@ void game_state_playing_update(engine::Engine &engine, Game &game, float t, floa
 }
 
 void game_state_playing_render(engine::Engine &engine, Game &game) {
+    if (ctx && update_func) {
+        ctx->Prepare(render_func);
+        ctx->SetArgAddress(0, &engine);
+        ctx->SetArgAddress(1, &game);
+        int r = ctx->Execute();
+        if (r != asEXECUTION_FINISHED) {
+            log_fatal("Could not execute render() in scripts/script.as: %s", ctx->GetExceptionString());
+        }
+        ctx->Unprepare();
+    }
 }
 
 void game_state_playing_render_imgui(engine::Engine &engine, Game &game) {
+    if (ctx && update_func) {
+        ctx->Prepare(render_imgui_func);
+        ctx->SetArgAddress(0, &engine);
+        ctx->SetArgAddress(1, &game);
+        int r = ctx->Execute();
+        if (r != asEXECUTION_FINISHED) {
+            log_fatal("Could not execute render_imgui() in scripts/script.as: %s", ctx->GetExceptionString());
+        }
+        ctx->Unprepare();
+    }
 }
 
 } // namespace game
